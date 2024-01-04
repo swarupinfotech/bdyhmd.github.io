bc757b429442fafdadd100e46785c4614aeb1cc7
Swarup Mahato <115489346+swarupinfotech@users.noreply.github.com>
2024-01-04 22:21:09 +0530

    bdy

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "hammad",
+        name: "Hammad",
+        message: "Hello, I'm Hammad",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "Hammad",
+        name: "Hammad",
+        message: "Hello, I'm Hammad",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
